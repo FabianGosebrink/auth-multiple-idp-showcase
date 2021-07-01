7c8e8d0265a27b2e4fd72c3ff9c04e79fe1edb52
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2021-07-01 19:28:46 +0200

    added mutliple IDPs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  ConfigAuthenticated,
+  ConfigUserData,
+  OidcSecurityService,
+} from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'auth-multiple-idp-showcase';
+
+  aadResult: any;
+  auth0Result: any;
+  allAuth: ConfigAuthenticated[] = [];
+  allUserData: ConfigUserData[] = [];
+
+  constructor(private oidcSecurityService: OidcSecurityService) {}
+
+  ngOnInit() {
+    this.oidcSecurityService.isAuthenticated$.subscribe(
+      ({ allConfigsAuthenticated }) => (this.allAuth = allConfigsAuthenticated)
+    );
+
+    this.oidcSecurityService.userData$.subscribe(
+      ({ allUserData }) => (this.allUserData = allUserData)
+    );
+
+    this.oidcSecurityService.checkAuthMultiple().subscribe((result) => {
+      this.auth0Result = result.find((x) => x.configId === 'auth0-config');
+
+      this.aadResult = result.find((x) => x.configId === 'aad-config');
+    });
+  }
+
+  loginAuth0() {
+    this.oidcSecurityService.authorize('auth0-config');
+  }
+
+  logoutAuth0() {
+    this.oidcSecurityService.logoff('auth0-config');
+  }
+
+  loginAad() {
+    this.oidcSecurityService.authorize('aad-config');
+  }
+
+  logoutAad() {
+    this.oidcSecurityService.logoff('aad-config');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,66 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    RouterModule.forRoot([]),
+    AuthModule.forRoot({
+      config: [
+        {
+          configId: 'auth0-config',
+          stsServer: 'https://dev-damienbod.eu.auth0.com',
+          redirectUrl: window.location.origin,
+          postLogoutRedirectUri: window.location.origin,
+          clientId: 'Ujh5oSBAFr1BuilgkZPcMWEgnuREgrwU',
+          scope: 'openid profile offline_access auth0-user-api-spa',
+          responseType: 'code',
+          silentRenew: true,
+          useRefreshToken: true,
+          logLevel: LogLevel.Debug,
+          customParamsAuthRequest: {
+            audience: 'https://auth0-api-spa',
+          },
+          customParamsRefreshTokenRequest: {
+            scope: 'openid profile offline_access auth0-user-api-spa',
+          },
+        },
+        {
+          configId: 'aad-config',
+          stsServer:
+            'https://login.microsoftonline.com/7ff95b15-dc21-4ba6-bc92-824856578fc1/v2.0',
+          authWellknownEndpointUrl:
+            'https://login.microsoftonline.com/common/v2.0',
+          redirectUrl: window.location.origin,
+          clientId: 'e38ea64a-2962-4cde-bfe7-dd2822fdab32',
+          scope:
+            'openid profile offline_access email api://e38ea64a-2962-4cde-bfe7-dd2822fdab32/access_as_user',
+          responseType: 'code',
+          silentRenew: true,
+          maxIdTokenIatOffsetAllowedInSeconds: 600,
+          issValidationOff: true,
+          autoUserInfo: false,
+          // silentRenewUrl: window.location.origin + '/silent-renew.html',
+          useRefreshToken: true,
+          logLevel: LogLevel.Debug,
+          customParamsAuthRequest: {
+            prompt: 'select_account', // login, consent
+          },
+        },
+      ],
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
